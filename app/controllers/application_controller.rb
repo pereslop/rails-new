884a9477549085782c5681f6f868897764f81b17
Mykola Kaban <pereslop@gmail.com>
2017-09-19 19:42:11 +0300

    redirecting if user.admin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-  end
-
-  def admin
-    if current_user.status == 'admin'
-      render :admin
-    else
-      redirect_to root_path
-    end
   end
 end
