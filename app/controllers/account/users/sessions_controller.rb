c15fef31aff44c4dcc79763aa8971d50cef50ce0
Mykola Kaban <pereslop@gmail.com>
2017-09-27 18:25:09 +0300

    displaing current user posts in new template

---
+++ app/controllers/account/users/sessions_controller.rb
@@ -0,0 +1,30 @@
+class Account::Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  def create
+    @user = User.find_by!(email: params[:user][:email])
+
+    sign_in @user
+    redirect_to account_user_path(@user)
+  end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+
+
+end
