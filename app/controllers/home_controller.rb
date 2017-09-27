6f52da62278a7a832ff77652a8bc73e4fe96489d
Mykola Kaban <pereslop@gmail.com>
2017-09-27 16:38:01 +0300

    working on users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  def index
+  end
+
   def show
     @user = current_user
     @posts = @user.posts.paginate(page: params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  namespace :account do
-    get 'users/index'
-  end
-
-  namespace :account do
-    get 'users/show'
-  end
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'home/index'
