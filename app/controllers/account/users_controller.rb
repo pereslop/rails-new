1bf05a22a86f883599e488d011d29992ce9983d4
Mykola Kaban <pereslop@gmail.com>
2017-09-27 15:42:54 +0300

    new buttn at index path

--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -1,6 +1,8 @@
 class Account::UsersController < AccountController
   def index
-
+    @user = current_user
+    @posts = @user.posts.paginate(page: params[:page])
+    @post = @user.posts.build if @user
   end
 
   def show
