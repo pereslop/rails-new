f9773785f05f4dd95b9b8646dd392973bb9b0ec2
Mykola Kaban <pereslop@gmail.com>
2017-10-05 19:20:35 +0300

    template for comments

--- app/controllers/account/comments_controller.rb
+++ app/controllers/account/comments_controller.rb
@@ -1,5 +1,6 @@
 class Account::CommentsController < ApplicationController
  before_action :find_post
+ before_action :resource, only: [:destroy]
 
   def create
     @comment = @post.comments.create(comment_params)
@@ -7,6 +8,11 @@ class Account::CommentsController < ApplicationController
     flash[:danger] = "Comment #{@comment.errors.messages}" unless @comment.save
     redirect_to account_post_path(find_post)
   end
+
+ def destroy
+   @comment.destroy
+   redirect_to account_post_path(@post)
+ end
 
   private
 
@@ -16,5 +22,9 @@ class Account::CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def resource
+    @comment = @post.comments.find(params[:id])
   end
 end
