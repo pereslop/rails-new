469185190139f331142bd0ab70114d076d983ef3
Mykola Kaban <pereslop@gmail.com>
2017-10-19 19:34:44 +0300

    scroll with kaminary

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,7 +2,7 @@ class Account::PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @posts = collection.page(params[:page]).per(24)
+    @posts = collection.page(params[:page]).per(20)
   end
 
   def create
--- app/controllers/account/users_controller.rb
+++ app/controllers/account/users_controller.rb
@@ -5,7 +5,7 @@ class Account::UsersController < AccountController
 
   def show
     @user = resource
-    @posts = @user.posts.ordered.page(params[:page]).per(24)
+    @posts = @user.posts.ordered.page(params[:page]).per(20)
     @post = @user.posts.new
   end
 
