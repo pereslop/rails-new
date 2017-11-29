dec919c7332a3d46065033002fcfb4c5a49d68d6
Mykola Kaban <pereslop@gmail.com>
2017-11-29 10:57:41 +0200

    styles for auth and main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     redirect_to account_posts_path if current_user
+    @posts = Post.all.ordered.limit(24)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
 
   namespace :admin do
     root 'users#index'
